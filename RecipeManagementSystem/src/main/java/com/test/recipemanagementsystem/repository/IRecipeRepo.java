004d179dead5f7036ad2f2d3022919e41e2682b3
shirishjaiswal <shirishjaiswal03@gmail.com>
2023-06-09 15:25:19 +0530

    Update

--- RecipeManagementSystem/src/main/java/com/test/recipemanagementsystem/repository/IRecipeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.test.recipemanagementsystem.repository;
-
-import com.test.recipemanagementsystem.model.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRecipeRepo extends JpaRepository<Recipe, Integer> {
-}
